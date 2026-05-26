5009048149ec4c4ead8c46db61bfb3033dd7e898
mrohitth <mrohitth@alumni.cmu.edu>
2026-05-25 23:06:23 -0400

    fix: use hash location in wouter Router for static hosting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,14 @@
-// client/src/App.tsx
 import { Router, Route } from "wouter"
+import { useHashLocation } from "wouter/use-hash-location"
 import { queryClient } from "./lib/queryClient"
 import { QueryClientProvider } from "@tanstack/react-query"
 import { Toaster } from "@/components/ui/toaster"
 import { TooltipProvider } from "@/components/ui/tooltip"
 import Portfolio from "@/pages/portfolio"
 
-// Simple router without hash location for Replit
 function AppRouter() {
   return (
-    <Router>
+    <Router hook={useHashLocation}>
       <Route path="/" component={Portfolio} />
     </Router>
   )
